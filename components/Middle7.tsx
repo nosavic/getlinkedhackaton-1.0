c2a81f11484ecdee3d084000a4b22bc98dc22ab2
Victory Nosakhare <vicnosa20@gmail.com>
2023-09-22 12:59:27 +0100

    work

--- components/Middle7.tsx
+++ components/Middle7.tsx
@@ -15,7 +15,7 @@ function Middle5() {
     <main className="mt-[150px] mx-[40px] mb-[60px]">
       <div className="tablet:text-center phone:text-center flex justify-end tablet:justify-center phone:justify-center mb-[40px]  ">
         <div>
-          <p className="text-[20px] font-bold  ">
+          <p className="text-[35px] font-bold  ">
             Prizes <b className="text-[#D434FE]"> and Rewards</b>{" "}
           </p>
           <p>
